ec12f75a499e90c3a92cc81746cd5e9de0af4b2a
MOON:E <waiguogaosho@hotmail.com>
2023-01-09 17:33:20 +0900

    Update gitignore

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // 문자열은 singleQuote로 ("" -> '')
+  singleQuote: true,
+  
+  // 코드 마지막에 세미콜론 있게 formatting
+  semi: true,
+
+// 들여쓰기 2칸
+  tabWidth: 4,
+  
+  // 배열 키: 값 뒤에 항상 콤마를 붙이도록 formatting
+  trailingComma: 'all',
+
+  // 코드 한줄이 maximum 80칸
+  printWidth: 80,
+
+  // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략하게 formating
+  arrowParens: 'avoid',
+
+  // windows에 뜨는 'Delete cr' 에러 해결
+  endOfLine: 'auto'
+
+}
