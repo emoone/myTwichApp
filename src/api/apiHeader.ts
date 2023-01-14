aa979a1bab2b4bed472007eeb230e2889a320860
emoone <waiguogaosho@hotmail.com>
2023-01-14 17:49:47 +0900

    Update getGamesList

---
+++ src/api/apiHeader.ts
@@ -0,0 +1,69 @@
+import axios from 'axios';
+
+/**
+ * client ID: bw08sngb0uq76b2smo3i32cfkzurkn
+ * client secret: 444fy1vojj0kg29ehf5lwpxeqanuvx
+ * twich 토큰
+ */
+const CLIENT_ID = 'bw08sngb0uq76b2smo3i32cfkzurkn';
+const CLIENT_SECRET = '444fy1vojj0kg29ehf5lwpxeqanuvx';
+
+const apiInstance = axios.create({
+  baseURL: 'https://api.twitch.tv',
+  timeout: 5271123,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+});
+
+/**
+ * topRankingGamesList api
+ *
+ */
+
+const getTopRankGamesList = (accessToken: string) => {
+  apiInstance
+    .get('/helix/games/top', {
+      headers: {
+        'client-id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then(({ data }) => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('error is', error);
+    });
+};
+
+/**
+ * accsess token 받는 api
+ */
+const authoInstance = axios.create({
+  baseURL: 'https://id.twitch.tv',
+  timeout: 5271123,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+});
+const getAuthoToken = () => {
+  authoInstance
+    .post('/oauth2/token', {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: 'client_credentials',
+    })
+    .then(({ data }) => {
+      console.log(data);
+      getTopRankGamesList(data.access_token);
+    })
+    .catch(error => {
+      const { response } = error;
+      const { data } = response;
+      const { status, message } = data;
+      console.log(status, message);
+    });
+};
+
+export { getAuthoToken, getTopRankGamesList };
--- src/pages/home/HomeComponent.tsx
+++ src/pages/home/HomeComponent.tsx
@@ -1,12 +1,17 @@
+import { getAuthoToken } from '../../api/apiHeader';
+
 const HomeComponent = () => {
   return (
     <div>
-      The HOMEasdfasdfasdfa 강경원님
+      The HOMEasdfasdfasdfa
       <div className="parent" style={{}}>
         parent
         <div>children1</div>
         <div>children2</div>
       </div>
+      <button type="button" onClick={() => getAuthoToken()}>
+        getGamesList
+      </button>
     </div>
   );
 };
